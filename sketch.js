c4820072dfc54504352c5248e07134183ad9b76e
RC07-ux-code <76210482+RC07-ux-code@users.noreply.github.com>
2020-12-30 14:29:50 +0900

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,100 @@
+var gameState;
+
+var monkey, monkey_running
+var banana, bananaImage, obstacle, obstacleImage
+var FoodGroup, obstacleGroup
+var score
+
+function preload() {
+
+
+  monkey_running = loadAnimation("sprite_0.png", "sprite_1.png", "sprite_2.png", "sprite_3.png", "sprite_4.png", "sprite_5.png", "sprite_6.png", "sprite_7.png", "sprite_8.png")
+
+  bananaImage = loadImage("banana.png");
+  obstacleImage = loadImage("obstacle.png");
+
+}
+
+
+
+function setup() {
+
+  //createCanvas(400,400);
+
+  var survivalTime = 0;
+
+  monkey = createSprite(80, 315, 20, 20);
+  monkey.addAnimation("moving", monkey_running);
+  monkey.scale = 0.1;
+
+  ground = createSprite(400, 350, 900, 10);
+  ground.velocityX = -4;
+  ground.x = ground.width / 2;
+
+  FoodGroup = new Group();
+  obstaclesGroup = new Group();
+
+  score = 0;
+
+  gameState = "play";
+}
+
+
+function draw() {
+  background("white");
+
+  if(gameState === "play"){
+  
+  if (ground.x < 0) {
+    ground.x = ground.width / 2;
+  }
+  
+  if (keyDown("space")) {
+    monkey.velocityY = -12;
+  }
+  monkey.velocityY = monkey.velocityY + 0.8;
+  monkey.collide(ground);
+
+  food();
+  obstacles();
+  
+  if (obstaclesGroup.isTouching(monkey)) {
+    ground.velocityX = 0;
+    monkey.velocityY = 0;
+    obstaclesGroup.setVelocityXEach(0);
+    FoodGroup.setVelocityXEach(0);
+    obstaclesGroup.setLifetimeEach(-1);
+    FoodGroup.setLifetimeEach(-1);
+    gameState="end";
+  }
+    score = score+Math.round(getFrameRate()/60);
+  }
+  drawSprites();
+  
+  text("Survival time: "+score, 250, 75);
+}
+
+function food() {
+  if (frameCount % 80 === 0) {
+    banana = createSprite(600, 250, 40, 10);
+    banana.addImage(bananaImage);
+    banana.scale = 0.05
+    banana.y = random(120, 200);
+    banana.velocityX = -5;
+    banana.lifetime = 300;
+
+    FoodGroup.add(banana);
+  }
+}
+
+function obstacles() {
+  if (frameCount % 300 === 0) {
+    obstacle = createSprite(800, 320, 10, 40);
+    obstacle.addImage(obstacleImage);
+    obstacle.scale = 0.15;
+    obstacle.velocityX = -6;
+    obstacle.lifetime = 300;
+
+    obstaclesGroup.add(obstacle);
+  }
+}
